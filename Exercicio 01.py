5afb5b0ee193361e5ad79e8cc422891ca7c13383
rebecca-medeiros <rebeccamedeiros.sh@gmail.com>
2022-11-01 13:16:03 -0300

    Atualização de atividades

--- Exercicio 01.py
+++ Exercicio 01.py
@@ -2,4 +2,3 @@
 #1. Importe numpy como 'np' e imprima o número da versão instalada no ambiente (local ou colab)
 import numpy as np
 print(np.__version__)
-print('oi')
---
+++ Exercicio 02.py
@@ -0,0 +1,5 @@
+### Exercícios
+#2. Crie um *array* 1D com números de 0 a 9 e imprima o resultado.
+import numpy as np
+a = np.arange(10)
+print(a)
