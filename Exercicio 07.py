2df394a0d5af305f381bcd2bd8dbb7645f311570
Rebecca de Medeiros <rebeccamedeiros.sh@gmail.com>
2022-11-04 19:46:42 -0300

    Exercícios - Numpy - Formação Python & Dados - Ada

--- Exercicio 07.py
+++ Exercicio 07.py
@@ -1,5 +1,6 @@
 ### Exercícios
 #7. Crie um array 1D com 100 valores de ponto flutuante (float), converta-o em uma matriz 2D com 10 linhas e 10 colunas.
 import numpy as np
-a = np.random.rand(10,10)
+a = np.arange(0,100,dtype=float)
+a = a.reshape(10,10)
 print(a)
